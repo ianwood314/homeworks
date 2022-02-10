435dc68bb9f124b18535c4edd53741fc89c32210
ianwood314 <93020229+ianwood314@users.noreply.github.com>
2022-02-09 20:05:45 -0600

    Added docstrings to all functions

--- homework02/generate_meteor_site_data.py
+++ homework02/generate_meteor_site_data.py
@@ -2,6 +2,17 @@ import random
 import json
 
 def generate_site_data(number_of_sites, latitude_interval, longitude_interval, composition_types):
+    """
+    Generates random meteor site data, like location and composotion, and returns the data as a dictionary.
+    
+    Args:
+        number_of_sites (int): the number of meteor sites to randomly generate
+        latitude_interval (list): a sorted list of two float numbers describing the lower and upper range of latitude values
+        longitude_interval (list): a sorted list of two float numbers describing the lower and upper range of longitude values
+        
+    Returns:
+        site_data (dict): contains a list of dictionaries where each dictionary contains the randmonly generated data for a meteor site
+    """
     site_data = {}
     site_data['sites'] = [] 
     for site_id in range(number_of_sites):
@@ -10,10 +21,20 @@ def generate_site_data(number_of_sites, latitude_interval, longitude_interval, c
     
     return site_data    
 
-def rand_num_between(interval_list): # returns a random float between the given interval values
+def rand_num_between(interval_list): 
+    """
+    Takes a sorted list of two floats and returns a random number between the given interval values.
+    
+    Arg:
+        interval_list (list): a sorted list of two float numbers describing the lower and upper range of possible values
+    """
     return random.uniform(interval_list[0], interval_list[1])
 
 def main():
+    """
+    Generates a JSON file containing the randomly generated meteor site data using parameters given by the user, like the number of sites, 
+    the range of possible latitude and logitude values, and the composition of the meteor.
+    """
     number_of_sites = 5
     latitude_interval = [16., 18.]
     longitude_interval = [82., 84.]
